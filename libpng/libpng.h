b30275497ab1a61a70ba0d015faf4346f2e1b76a
Alexander Zautke <mail@alexander-zautke.com>
2016-03-25 16:26:54 +0100

    Initial Commit

---
+++ libpng/libpng.h
@@ -0,0 +1,13 @@
+//
+//  libpng.h
+//  libpng
+//
+//  Created by Alexander Zautke on 25/03/16.
+//  Copyright © 2016 moep-software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libpng : NSObject
+
+@end
---
+++ libpng/libpng.m
@@ -0,0 +1,13 @@
+//
+//  libpng.m
+//  libpng
+//
+//  Created by Alexander Zautke on 25/03/16.
+//  Copyright © 2016 moep-software. All rights reserved.
+//
+
+#import "libpng.h"
+
+@implementation libpng
+
+@end
